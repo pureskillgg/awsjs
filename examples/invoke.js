eccb76b382874997277387350bf8092bf8de8004
Evan Sosenko <evan@pureskill.gg>
2021-07-07 21:33:52 -0700

    Run format

--- examples/invoke.js
+++ examples/invoke.js
@@ -3,14 +3,16 @@ import path from 'path'
 
 import { LambdaClient } from '../index.js'
 
-export const invoke = ({ log }) => async (functionName, req) => {
-  const input = await readJson('tmp', `${req}.json`)
-  const client = new LambdaClient({
-    functionName,
-    log
-  })
-  return client.invokeJson(input)
-}
+export const invoke =
+  ({ log }) =>
+  async (functionName, req) => {
+    const input = await readJson('tmp', `${req}.json`)
+    const client = new LambdaClient({
+      functionName,
+      log
+    })
+    return client.invokeJson(input)
+  }
 
 const readJson = async (...args) => {
   const src = path.resolve(...args)
