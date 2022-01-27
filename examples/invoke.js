3c930d2d6d92321c44049fa76974cb0610a0655c
Evan Sosenko <evan@pureskill.gg>
2022-01-27 14:34:23 -0800

    Use fsPromises

--- examples/invoke.js
+++ examples/invoke.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import { promises as fsPromises } from 'fs'
 import path from 'path'
 
 import { LambdaClient } from '../index.js'
@@ -16,6 +16,6 @@ export const invoke =
 
 const readJson = async (...args) => {
   const src = path.resolve(...args)
-  const data = await fs.promises.readFile(src)
+  const data = await fsPromises.readFile(src)
   return JSON.parse(data.toString())
 }
