de3c8b4422f439826738556b13671ed9a8821612
Evan Sosenko <evan@pureskill.gg>
2024-06-09 18:29:50 -0500

    Bind groupName to SchedulerClient

--- examples/schedule.js
+++ examples/schedule.js
@@ -4,13 +4,13 @@ export const createSchedule =
   ({ log }) =>
   async (scheduleName, groupName, arn, roleArn) => {
     const client = new SchedulerClient({
+      groupName,
       log
     })
     return client.createSchedule(scheduleName, {
       scheduleExpression: 'rate(1 minute)',
       flexibleTimeWindow: { mode: 'OFF' },
       input: { foo: 'bar' },
-      groupName,
       target: {
         arn,
         roleArn,
@@ -26,22 +26,23 @@ export const deleteSchedule =
   ({ log }) =>
   async (scheduleName, groupName) => {
     const client = new SchedulerClient({
+      groupName,
       log
     })
-    return client.deleteSchedule(scheduleName, { groupName })
+    return client.deleteSchedule(scheduleName)
   }
 
 export const updateSchedule =
   ({ log }) =>
   async (scheduleName, groupName, arn, roleArn) => {
     const client = new SchedulerClient({
+      groupName,
       log
     })
     return client.updateSchedule(scheduleName, {
       scheduleExpression: 'rate(2 minutes)',
       flexibleTimeWindow: { mode: 'OFF' },
       input: { foo: 'bar' },
-      groupName,
       target: {
         arn,
         roleArn,
@@ -57,7 +58,8 @@ export const getSchedule =
   ({ log }) =>
   async (scheduleName, groupName) => {
     const client = new SchedulerClient({
+      groupName,
       log
     })
-    return client.getSchedule(scheduleName, { groupName })
+    return client.getSchedule(scheduleName)
   }
